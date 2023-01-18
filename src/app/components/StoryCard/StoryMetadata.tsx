9734c623275b83b6af4c23295c951d968150960e
Ming Fe <mingfework@gmail.com>
2023-01-18 10:02:53 +0700

    feat: update test

--- src/app/components/StoryCard/StoryMetadata.tsx
+++ src/app/components/StoryCard/StoryMetadata.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import { BsFillStarFill } from 'react-icons/bs'
-import { useTranslation } from 'react-i18next'
 import 'twin.macro'
 
 // Utils
 import { formatTime } from '@/utils/formatTime'
-import { messages } from './messages'
 
 interface StoryMetadataProps {
   author: string
@@ -14,11 +12,12 @@ interface StoryMetadataProps {
 }
 
 export const StoryMetadata: React.FC<StoryMetadataProps> = props => {
-  const { t } = useTranslation()
   const { author, time, score } = props
   return (
     <div tw="flex items-center justify-between">
-      <div tw="text-xs text-textGrey">{t(messages.postedBy(), { author, time: formatTime(time as number) })}</div>
+      <div tw="text-xs text-textGrey">
+        Posted by {author} • {formatTime(time as number)}
+      </div>
       <div tw="flex items-center">
         <BsFillStarFill color="gold" tw="mr-[6px]" />
         <div tw="text-xs sm:text-sm text-white font-semibold mt-[2px]">{score}</div>
--- src/app/components/StoryCard/messages.ts
+++ src/app/components/StoryCard/messages.ts
@@ -2,6 +2,5 @@ import { translations } from '@/locales/translations'
 import { _t } from '@/utils/messages'
 
 export const messages = {
-  postedBy: () => _t(translations.postedBy),
   totalComments: () => _t(translations.totalCommments)
 }
